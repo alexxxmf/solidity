af3acae064ffb3b08312dc7161899d647091c4cc
alejandro <amoro@alumni.unav.es>
2021-09-26 10:59:18 +0100

    added more files plus mock deployment

---
+++ lottery/scripts/deploy_mocks.py
@@ -0,0 +1,19 @@
+from brownie import LinkToken, MockV3Aggregator, MockVRFCoordinator
+
+from scripts.helpers import get_account
+
+
+DECIMALS = 8
+INITIAL_VALUE = 200000000000
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+  account = get_account()
+
+  MockV3Aggregator.deploy(decimals, initial_value, {"from": account})
+  link_token = LinkToken.deploy({"from": account})
+  MockVRFCoordinator.deploy(link_token.address, {"from": account})
+
+  print("Mocks for price feed, link token and VRF Coordinator have been successfully deployed")
+
+def main():
+  deploy_mocks()

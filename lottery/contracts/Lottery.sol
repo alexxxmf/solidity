5d65bc607a357d3be000950826ae7a461690bfbf
alejandro <amoro@alumni.unav.es>
2021-09-27 11:38:13 +0100

    improving constructor function

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 
-contract Lottery is Ownable {
+contract Lottery is Ownable, VRFConsumerBase {
   address payable[] public players;
   address payable public recentWinner;
   uint256 public randomness;
@@ -17,11 +18,23 @@ contract Lottery is Ownable {
     CALCULATING_WINNER
   }
   LOTTERY_STATE public lottery_state;
+  uint256 public fee;
+  bytes32 public keyhash;
+  event RequestedRandomness(bytes32 requestId);
   
-  constructor(address _priceFeedAddress, uint256 _usdEntryFee) public{
+  constructor(
+    address _priceFeedAddress,
+    uint256 _usdEntryFee,
+    address _vrfCoordinator,
+    address _link,
+    uint256 _fee,
+    bytes32 _keyhash
+  ) public VRFConsumerBase(_vrfCoordinator, _link){
     ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
     usdEntryFee = _usdEntryFee * (10 ** 18); // we pass it in usd then we convert to 18 decimals
     lottery_state = LOTTERY_STATE.CLOSED;
+    fee = _fee;
+    keyhash = _keyhash;
   }
 
   function enter() public payable {
@@ -55,6 +68,7 @@ contract Lottery is Ownable {
   }
 
   function endLottery() public onlyOwner {
+    lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
 
   }
 

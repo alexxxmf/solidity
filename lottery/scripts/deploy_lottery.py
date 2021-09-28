46be795e9206a00304c106fb6175a79a20c6e46f
alejandro <amoro@alumni.unav.es>
2021-09-28 10:15:23 +0100

    fixed constructor for lottery. Some stuff missing there

--- lottery/scripts/deploy_lottery.py
+++ lottery/scripts/deploy_lottery.py
@@ -9,7 +9,16 @@ def deploy_lottery(entrance_fee=ENTRANCE_FEE):
   account = get_account()
   priceFeedAddress = get_contract("eth_usd_price_feed").address
 
-  lottery_contract = Lottery.deploy(priceFeedAddress, entrance_fee, {"from": account});
+  lottery_contract = Lottery.deploy(
+    priceFeedAddress,
+    entrance_fee,
+    get_contract("vrf_coordinator").address,
+    get_contract("link_token").address,
+    config["networks"][network.show_active()]["fee"],
+    config["networks"][network.show_active()]["keyhash"],
+    {"from": account},
+    publish_source=config["networks"][network.show_active()].get("verify", False),
+  );
 
   return lottery_contract
 

666a58a8dc0bc69a57e21102f59f87d4733c95f3
alejandro <amoro@alumni.unav.es>
2021-09-30 10:43:28 +0100

    playing with aave

--- aave_playground/scripts/aave_borrow.py
+++ aave_playground/scripts/aave_borrow.py
@@ -3,7 +3,7 @@ from brownie import (
   config,
   interface,
   network,
-  interface,
+  interface
 )
 from web3 import Web3
 
@@ -21,6 +21,60 @@ def get_lending_pool():
   lending_pool = interface.ILendingPool(lending_pool_address)
   return lending_pool
 
+def approve_erc20(amount, lending_pool_address, erc20_address, account):
+  print("Approving ERC20...")
+  erc20 = interface.ERC20(erc20_address)
+  tx_hash = erc20.approve(lending_pool_address, amount, {"from": account})
+  tx_hash.wait(1)
+  print("Approved!")
+  return True
+
+def get_borrowable_data(lending_pool, account):
+  (
+    total_collateral_eth,
+    total_debt_eth,
+    available_borrow_eth,
+    current_liquidation_threshold,
+    tlv,
+    health_factor
+  ) = lending_pool.getUserAccountData(account.address)
+
+  available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+  total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+  total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+  print(f"You have {total_collateral_eth} worth of ETH deposited.")
+  print(f"You have {total_debt_eth} worth of ETH borrowed.")
+  print(f"You can borrow {available_borrow_eth} worth of ETH.")
+  return (float(available_borrow_eth), float(total_debt_eth))
+
+def get_asset_price():
+  dai_eth_price_feed = interface.AggregatorV3Interface(
+      config["networks"][network.show_active()]["dai_eth_price_feed"]
+  )
+  latest_price = Web3.fromWei(dai_eth_price_feed.latestRoundData()[1], "ether")
+  print(f"The DAI/ETH price is {latest_price}")
+  return float(latest_price)
+
+def repay_all(amount, lending_pool, account):
+  approve_erc20(
+    Web3.toWei(amount, "ether"),
+    lending_pool,
+    config["networks"][network.show_active()]["aave_dai_token"],
+    account,
+  )
+
 def main():
+  account = get_account()
+  erc20_address = config["networks"][network.show_active()]["weth_token"]
+
+  if network.show_active() in ["mainnet-fork", "mainnet-fork-dev"]:
+    get_weth(account=account)
+
+  erc20 = interface.ERC20(config["networks"][network.show_active()]["weth_token"])
+
   lending_pool = get_lending_pool()
-  print(lending_pool)
+  approve_erc20(amount, lending_pool.address, erc20_address, account)
+  print("Depositing...")
+  lending_pool.deposit(erc20_address, amount, account.address, 0, {"from": account})
+  print("Deposited!")
+  borrowable_eth, total_debt_eth = get_borrowable_data(lending_pool, account)
--- aave_playground/scripts/get_weth.py
+++ aave_playground/scripts/get_weth.py
@@ -1,16 +1,18 @@
-from brownie import interface, config, network
+from brownie import interface, config, network, accounts
 
 from scripts.helpers import get_account
 
 
-def get_weth():
-  account = get_account()
-  # Addr:  0xc778417E063141139Fce010982780140Aa0cD5Ab
-  # ABI: WethInterface
-  weth_contract = interface.WethInterface(config["networks"][network.show_active()]["weth_token"])
-  tx = weth_contract.deposit({"from": account, "value": 0.1 * (10 ** 18)})
+def get_weth(account=None):
+  account = (
+      account if account else accounts.add(config["wallets"]["from_key"])
+  )  # add your keystore ID as an argument to this call
+  weth = interface.WethInterface(
+      config["networks"][network.show_active()]["weth_token"]
+  )
+  tx = weth.deposit({"from": account, "value": 0.1 * 1e18})
   tx.wait(1)
-  print("ETH sent!")
+  print("Received 0.1 WETH")
   return tx
 
 def main():

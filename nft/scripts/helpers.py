024920fef432643841a3eb8023215c0725f8d0b5
alejandro <amoro@alumni.unav.es>
2021-10-03 10:25:12 +0100

    added another piece of utilitiy

--- nft/scripts/helpers.py
+++ nft/scripts/helpers.py
@@ -6,6 +6,8 @@ from brownie import (
   VRFCoordinatorMock,
   Contract
 )
+from web3 import Web3
+
 from scripts.deploy_mocks import deploy_mocks
 
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
@@ -45,3 +47,13 @@ def get_contract(contract_name):
         contract_type._name, contract_address, contract_type.abi
     )
   return contract
+
+def fund_with_link(
+    contract_address, account=None, link_token=None, amount=Web3.toWei(0.3, "ether")
+):
+  account = account if account else get_account()
+  link_token = link_token if link_token else get_contract("link_token")
+  funding_tx = link_token.transfer(contract_address, amount, {"from": account})
+  funding_tx.wait(1)
+  print(f"Funded {contract_address}")
+  return funding_tx
